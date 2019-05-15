1a94755382251460226de82bb5fe548d606c40d6
havardhovde <haavardhovde@gmail.com>
2019-05-16 00:01:57 +0200

    updated shuffle, still wip

--- js/shuffle/script.js
+++ js/shuffle/script.js
@@ -4,11 +4,13 @@ let img = [
     {url: "http://www.clker.com/cliparts/b/e/c/3/131406375432193858green%20square-hi.png", name: "greenSquare"},
     {url: "http://www.clker.com/cliparts/s/A/a/2/m/J/yellow-square-hi.png", name: "yellowSquare"},
     {url: "https://pitshanger-ltd.co.uk/images/colours/566-Plum%20262.jpg", name: "purpleSquare"},
-    {url: "http://www.clker.com/cliparts/4/2/X/T/o/0/blue-square.svg.hi.png", name: "blueSquare"},
-    {url: "http://www.clker.com/cliparts/9/W/y/p/N/w/red-square-hi.png", name: "redSquare"},
-    {url: "http://www.clker.com/cliparts/b/e/c/3/131406375432193858green%20square-hi.png", name: "greenSquare"},
-    {url: "http://www.clker.com/cliparts/s/A/a/2/m/J/yellow-square-hi.png", name: "yellowSquare"},
-    {url: "https://pitshanger-ltd.co.uk/images/colours/566-Plum%20262.jpg", name: "purpleSquare"},
+    {url: "https://i.imgur.com/9yILi61.png", name: "orangeSquare"},
+    // {url: "http://www.clker.com/cliparts/4/2/X/T/o/0/blue-square.svg.hi.png", name: "blueSquare"},
+    // {url: "http://www.clker.com/cliparts/9/W/y/p/N/w/red-square-hi.png", name: "redSquare"},
+    // {url: "http://www.clker.com/cliparts/b/e/c/3/131406375432193858green%20square-hi.png", name: "greenSquare"},
+    // {url: "http://www.clker.com/cliparts/s/A/a/2/m/J/yellow-square-hi.png", name: "yellowSquare"},
+    // {url: "https://pitshanger-ltd.co.uk/images/colours/566-Plum%20262.jpg", name: "purpleSquare"},
+    // {url: "https://i.imgur.com/9yILi61.png", name: "orangeSquare"},
 ]
 
 function shuffle(array) {
@@ -20,6 +22,10 @@ function shuffle(array) {
 
 shuffle(img)
 
-for(let i = 0; i <= 8; i++) {
-    document.querySelector(`#img${i}`).innerHTML = `<img src="${img[i].url}"></img>`;
+for(let i = 0; i <= 11; i++) {
+    if(i <= 5) {
+        document.querySelector(`#img${i}`).innerHTML = `<img src="${img[i].url}"></img>`;
+    } if(i >= 5) {
+        document.querySelector(`#img${i}`).innerHTML = `<img src="${img[i - 5].url}"></img>`;
+    }
 }
