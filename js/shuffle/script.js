bdbf0adbb68933233fc60690fc83a4361588ca17
havardhovde <haavardhovde@gmail.com>
2019-05-16 10:35:30 +0200

    shuffle done

--- js/shuffle/script.js
+++ js/shuffle/script.js
@@ -1,11 +1,13 @@
-let img = [
+let img1 = [
     {src: "img/blueSquare.png", name: "blueSquare"},
     {src: "img/redSquare.png", name: "redSquare"},
     {src: "img/greenSquare.png", name: "greenSquare"},
     {src: "img/yellowSquare.png", name: "yellowSquare"},
     {src: "img/purpleSquare.png", name: "purpleSquare"},
     {src: "img/orangeSquare.png", name: "orangeSquare"},
-]
+];
+
+let img2 = [];
 
 function shuffle(array) {
     for (let i = array.length - 1; i > 0; i--) {
@@ -14,12 +16,17 @@ function shuffle(array) {
     }
 }
 
-shuffle(img)
+shuffle(img1)
 
 for(let i = 0; i <= 11; i++) {
     if(i <= 5) {
-        document.querySelector(`#img${i}`).innerHTML = `<img src="${img[i].src}"></img>`;
-    } else if(i > 5) {
-        document.querySelector(`#img${i}`).innerHTML = `<img src="${img[i - 6].src}"></img>`;
+        document.querySelector(`#img${i}`).innerHTML = `<img src="${img1[0].src}"></img>`;
+        img2.push(img1.splice(0,1)[0]);
+        
+    } 
+    shuffle(img2);
+    if(i > 5) {
+        document.querySelector(`#img${i}`).innerHTML = `<img src="${img2[0].src}"></img>`;
+        img1.push(img2.splice(0,1)[0]);
     }
 }
