efb084f2ef39616629bd263346478d2a5153349a
havardhovde <haavardhovde@gmail.com>
2019-05-23 14:50:10 +0200

    more (bad) progress

--- react-native/temaoppgave/components/MemoryGame.js
+++ react-native/temaoppgave/components/MemoryGame.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {StyleSheet, View, Text, Image, TouchableHighlight}  from "react-native"
+import {StyleSheet, View, Text, Image, TouchableOpacity}  from "react-native"
 
 const Styles = StyleSheet.create({
     memory: {
@@ -11,49 +11,97 @@ const Styles = StyleSheet.create({
         justifyContent: "center",
         alignItems: "center",
     },
-    img: {
+    btn: {
         width: 75,
         height: 75,
         margin: 10,
-        borderWidth: 6,
+        borderWidth: 5,
         borderRadius: 10,
-        borderColor: "blackx",
-    }
+        borderColor: "black",
+    },
+    img: {
+        width: 65,
+        height: 65,
+    } 
 });
 
 export default class MemoryGame extends React.Component {
-    render() {
-        let img1 = [
-            {src: require("../img/blueSquare.png"), name: "blueSquare"},
-            {src: require("../img/redSquare.png"), name: "redSquare"},
-            {src: require("../img/greenSquare.png"), name: "greenSquare"},
-            {src: require("../img/yellowSquare.png"), name: "yellowSquare"},
-            {src: require("../img/purpleSquare.png"), name: "purpleSquare"},
-            {src: require("../img/orangeSquare.png"), name: "orangeSquare"},
-        ];
-    
-        img1 = img1.concat(img1);
+    constructor(props){
+        super(props);
+        this.state = {
+            image: require("../img/questionmark.png"),
+        };
+        this.changeImage = this.changeImage.bind(this);
+    }
 
-        function shuffle(array){
-            for(let i = array.length -1; i > 0; i--) {
+    shuffle = (array) =>{
+        for(let i = array.length -1; i > 0; i--) {
             let j = Math.floor(Math.random() * (i + 1));
             [array[i], array[j]] = [array[j], array[i]]
-            }
         }
+    }
+
+    changeImage = () => {
+        // this.setState({
+        //     image: require("../img/redSquare.png")
+        // })
+        console.log("test")
+    }
+
     
-        shuffle(img1)
+    img1 = [
+        {src: require("../img/blueSquare.png"), name: "blueSquare", id: 0},
+        {src: require("../img/redSquare.png"), name: "redSquare", id: 1},
+        {src: require("../img/greenSquare.png"), name: "greenSquare", id: 2},
+        {src: require("../img/yellowSquare.png"), name: "yellowSquare", id: 3},
+        {src: require("../img/purpleSquare.png"), name: "purpleSquare", id: 4},
+        {src: require("../img/orangeSquare.png"), name: "orangeSquare", id: 5},
+    ];
 
-        let boxes = [];
+    // img1 = img1.concat(img1)
 
-        for(let i = 0; i <= 11; i++) {
-            boxes.push(
-                <Image key={"box_" + i} style={Styles.img} source= {img1[i].src} />
-            )
-        }
+    img2 = this.img1.map(box => ({src: box.src, name: box.name, id: box.id + "x"}))
+
+    img3 = [...this.img1,...this.img2]
+
+    render() {
+
+        // let img1 = [
+        //     {src: require("../img/blueSquare.png"), name: "blueSquare", id: 0},
+        //     {src: require("../img/redSquare.png"), name: "redSquare", id: 1},
+        //     {src: require("../img/greenSquare.png"), name: "greenSquare", id: 2},
+        //     {src: require("../img/yellowSquare.png"), name: "yellowSquare", id: 3},
+        //     {src: require("../img/purpleSquare.png"), name: "purpleSquare", id: 4},
+        //     {src: require("../img/orangeSquare.png"), name: "orangeSquare", id: 5},
+        // ];
+    
+        // // img1 = img1.concat(img1)
+
+        // let img2 = img1.map(box => ({src: box.src, name: box.name, id: box.id + "x"}))
+
+        // let img3 = [...img1,...img2]
+
+        this.shuffle(this.img3)
+
+        let boxTest = this.img3.map(imageBox => 
+            <TouchableOpacity key={imageBox.id} onPress={this.changeImage} style={Styles.btn}>
+                <Image style={Styles.img} source={this.state.image} />
+            </TouchableOpacity>
+        );
+
+        console.log(boxTest.name)
+
+        // for(let i = 0; i <= 11; i++) {
+        //     boxes.push(
+        //         <TouchableHighlight onPress={console.log("test")} key={"box_" + i} style={Styles.btn}>
+        //             <Image style={Styles.img} source={this.state.image} />
+        //         </TouchableHighlight>
+        //     )
+        // }
 
         return (
             <View style={Styles.memory}>
-                {boxes}
+                {boxTest}
             </View>  
         );
     }
