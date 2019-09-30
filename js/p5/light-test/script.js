7deda95e06eb3f225a17bf5bc50a6e042266501a
havardhovde <haavardhovde@gmail.com>
2019-09-30 19:32:54 +0200

    started second p5js light test

--- js/p5/light-test/script.js
+++ js/p5/light-test/script.js
@@ -6,7 +6,7 @@ function draw() {
     background(0);
     let locX = mouseX - width / 2;
     let locY = mouseY - height / 2;
-    pointLight(255, 255, 255, locX, locY, 15);
+    pointLight(255, 255, 255, locX, locY, 30);
     noStroke();
 
     for (let x = 0; x <= width; x = x + 60 ) {
